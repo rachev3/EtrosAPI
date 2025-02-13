5c34da908859e6f548f5158a649acfff4fae5e95
rachev3 <ddrachev123@gmail.com>
2025-02-13 21:24:30 +0200

    Add MongoDB dependency and refactor upload middleware

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from "../middlewares/upload.js";
+import upload from "../middleware/uploadMiddleware.js";
 import { uploadPhoto } from "../controllers/uploadController.js";
 
 const router = express.Router();
